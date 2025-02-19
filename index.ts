86718f3fa961bfbb658465e4aeadcb2369dc8ee0
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-02-19 14:33:47 +0100

    u6

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { writeFileSync } from "fs";
+import loadSboms from "./src/loadSboms";
+import MergedSbom from "./src/MergedSbom";
+import core from "@actions/core";
+
+const filename = core.getInput("filename") ? core.getInput("filename") : "dependencies.sbom.json";
+const reposString = core.getInput("repos");
+
+export const run = async () => {
+    try {
+        core.info("=== 1. Loading SBOMs ===");
+        const repos = reposString !== "" ? reposString.split(";") : ["hpi-schul-cloud/tldraw-server:999.6.6"];
+        const sboms = await loadSboms(filename, repos);
+
+        core.info("=== 2. Generating merged SBOM ===");
+        const mergedSbom = new MergedSbom(sboms);
+        writeFileSync(`svs-sbom.json`, mergedSbom.toString());
+
+        core.setOutput("json", mergedSbom.toString());
+    } catch (error: any) {
+        const message = "message" in error ? error.nessage : error;
+        core.error(message);
+    }
+};
